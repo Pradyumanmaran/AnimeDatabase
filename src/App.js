4e789b64c7baa55727e56764c377916c5718d3a0
Pradyumanmaran <pradyumanmaran@gmail.com>
2024-03-11 20:10:08 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect,useReducer } from "react";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import MainContent from "./components/MainContent";
+import Favourite from "./components/Favourite";
 
 function App() {
+  // const initialValue = []
+  const [animeList, SetAnimeList] = useState([]);
+  const [topAnime, SetTopAnime] = useState([]);
+  const [search, SetSearch] = useState("");
+  // const [favourite, dispatchState] = useReducer(useFav, initialValue)
+  
+  // function useFav(fav, action) {
+  //   console.log(fav)
+  //   console.log(action)
+  //   // console.log(favourite)
+     
+  //   switch (action.type) {
+  //     case "ADD_FAV":
+  //       return [...fav, action.payload];
+  //     case "REMOVE_FAV":
+  //       return fav.filter((favu) => favu.id !== action.payload);
+  //     default:
+  //       return fav;
+  //   }
+
+  // }
+  const GetTopAnime = async () => {
+    const temp = await fetch(`https://api.jikan.moe/v4/top/anime`)
+      .then((res) => res.json())
+      .then((data) => data.data);
+    SetTopAnime(temp.slice(0, 6));
+  };
+
+  const HandleSearch = (e) => {
+    e.preventDefault();
+    FetchAnime(search);
+  };
+  const FetchAnime = async (query) => {
+    const temp = await fetch(
+      `https://api.jikan.moe/v4/anime?q=${query}&sfw`
+    ).then((res) => res.json());
+    SetAnimeList(temp.data);
+
+    console.log(temp.data);
+    console.log(animeList);
+  };
+  useEffect(() => {
+    GetTopAnime();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Header />
+        <div className="content-wrap">
+          <Sidebar topAnime={topAnime} />
+          <MainContent
+            HandleSearch={HandleSearch}
+            search={search}
+            SetSearch={SetSearch}
+            animeList={animeList}
+            topAnime={topAnime}
+            // dispatchState={dispatchState}
+            // favourite={favourite}
+          />
+          
+         
+        </div>
+      </div>
+    </>
   );
 }
 
